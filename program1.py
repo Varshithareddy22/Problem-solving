61b9ad0182a4c28d50ca5ce3ab0b2ae6466cb551
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-11 12:08:43 +0530

    first commit

--- program1.py
+++ program1.py
@@ -1,3 +1,10 @@
+#storing of name,age & branch
+name = "Varshitha"
+age = 21
+branch = "CSM" 
+print(name,age,branch)
+
+#calculator 
 a=int(input("enter the number"))
 b=int(input("enter the number"))
 print(a+b)
