c78fa98dd8f44183b2cf7cd58be9a88d8c1135c9
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-11 12:22:43 +0530

    first commit

--- program1.py
+++ program1.py
@@ -1,13 +1,21 @@
 #storing of name,age & branch
+'''
 name = "Varshitha"
 age = 21
 branch = "CSM" 
-print(name,age,branch)
+print(name,age,branch)'''
 
 #calculator 
+'''
 a=int(input("enter the number"))
 b=int(input("enter the number"))
 print(a+b)
 print(a-b)
 print(a*b)
-print(a/b)
+print(a/b) '''
+
+#area of rectangle
+l=int(input("enter the length"))
+b=int(input("enter the breadth"))
+area_of_rectangle= l*b
+print(area_of_rectangle,"cm^2") 
