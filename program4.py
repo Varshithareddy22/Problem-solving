3184abe9ab53af3fd0107bdbc3204412b5b38d7c
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-12 22:42:02 +0530

    first commit

--- program4.py
+++ program4.py
@@ -4,3 +4,14 @@ a=pow(x, 2)
 b=pow(x, 3)
 print("square of the number is",a)
 print("cube of the number is",b)
+
+#calcuate the area of the square
+x= int(input("enter the side of the square"))
+area_of_square= x** 2
+print("Area of the square is",area_of_square)
+
+
+#calcuate the area of the cube 
+x= int(input("enter the side of the cube"))
+area_of_cube= 6*(x** 2)
+print("Area of the cube is",area_of_cube)
