578f4f802580be51add0b6a7dcd89bf34483b0f0
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-12 21:35:13 +0530

    first commit

---
+++ program3.py
@@ -0,0 +1,14 @@
+#1
+'''
+x= "Varshitha"
+y= 21
+print ("my name is", x , "and I am" , y , "years old.")
+'''
+
+#2
+'''
+x= int(input("enter current year"))
+y= int(input("enter your birth year"))
+a= x-y
+print("your age is",a)
+'''
