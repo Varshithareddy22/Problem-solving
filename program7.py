15e18c40e0e71b3a2f9d0190e0d04357786d0903
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-13 23:23:16 +0530

    first commit

--- program7.py
+++ program7.py
@@ -9,3 +9,26 @@ elif (y > x and y < z) or (y < x and y > z):
     print("y is the second largest number")
 else:
     print("z is the second largest number")
+
+#greatest and 2nd greatest value
+x = int(input("enter the value of x"))
+y = int(input("enter the value of y"))
+z = int(input("enter the value of z"))
+if x > y and x > z:
+    print("x is greater")
+    if y > z:
+        print("y is 2nd greatest value")
+    else:
+        print("z is 2nd greatest value")
+elif y > x and y > z:
+    print("y is greater")
+    if x > z:
+        print("x is 2nd greatest value")
+    else:
+        print("z is 2nd greatest value")
+else:
+    print("z is greater")
+    if x > y:
+        print("x is 2nd greatest value")
+    else:
+        print("y is 2nd greatest value")
