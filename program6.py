ecb6a6d999c5fbed7a944017435848fd774bf135
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-13 21:59:18 +0530

    first commit

---
+++ program6.py
@@ -0,0 +1,19 @@
+#divisible by 3 or 7
+'''
+x= int(input("enter the variable"))
+if x%3==0 :
+    print(x,"is divisible by 3")
+elif x%7==0 :
+    print(x,"is divisible by 7")
+else:
+    print(x,"is not divible 3 or 7")     
+'''
+
+#divisible by 5 and 11
+'''
+x= int(input("enter the variable"))
+if x%5==0 and x%11==0:
+    print(x,"is divisible by 5 and 11")
+else:
+    print(x,"is not divible 5 and 11") 
+'''
