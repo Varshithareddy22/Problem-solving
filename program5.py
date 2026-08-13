14ae067cee90db9c2042970cad19e4badd69c2eb
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-13 12:36:15 +0530

    first commit

---
+++ program5.py
@@ -0,0 +1,18 @@
+#1
+x= int(input("enter the voter age: "))
+if x>=18 :
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
+
+
+#2
+x= int(input("enter the value of x"))
+y= int(input("enter the value of y"))
+z= int(input("enter the value of z"))
+if (x>y) and (x>z):
+    print("x is greater")
+elif (y>x) and (y>z):
+    print("y is greater") 
+else:
+    print("z is greater")
