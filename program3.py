11f2987208f2610427bf3778ab134b5a947ebab4
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-13 23:27:50 +0530

    Update program3.py

--- program3.py
+++ program3.py
@@ -4,13 +4,13 @@ y= 21
 print("my name is", x , "and I am" , y , "years old.")
 
 #2
-x= int(input("enter current year"))
-y= int(input("enter your birth year"))
+x= int(input("enter current year:"))
+y= int(input("enter your birth year:"))
 a= x-y
 print("your age is",a)
 
 #3
-x= int(input("enter number"))
-y= int(input("enter number"))
+x= int(input("enter number:"))
+y= int(input("enter number:"))
 a= x*y
 print(a)
