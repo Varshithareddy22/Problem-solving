4362f46a140f9b52c18ad3f8fea91953a34c1b0c
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-13 22:04:41 +0530

    first commit

--- program6.py
+++ program6.py
@@ -1,5 +1,4 @@
 #divisible by 3 or 7
-'''
 x= int(input("enter the variable"))
 if x%3==0 :
     print(x,"is divisible by 3")
@@ -7,13 +6,17 @@ elif x%7==0 :
     print(x,"is divisible by 7")
 else:
     print(x,"is not divible 3 or 7")     
-'''
 
 #divisible by 5 and 11
-'''
 x= int(input("enter the variable"))
 if x%5==0 and x%11==0:
     print(x,"is divisible by 5 and 11")
 else:
     print(x,"is not divible 5 and 11") 
-'''
+
+#number lies between 10 and 50
+x= int(input("enter the variable"))
+if x>=10 and x<=50 :
+    print(x,"lies between 10 and 50")
+else :    
+    print(x,"doesn't lie between 10 and 50")
