5a7b9f5d5309c0d10ad9994d01e95dba6e1377f7
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-14 22:32:14 +0530

    first commit

--- program8.py
+++ program8.py
@@ -1,7 +1,16 @@
 #print 1 to 10
+'''
 for i in range (1,11):
-    print(i)
+    print(i)'''
 
 #print 10 to 1
+'''
 for i in range (10,0,-1):
     print(i)
+'''
+
+#print even numbers 2 to 20
+'''
+for i in range (2,21,+2):
+    print(i)
+    '''
