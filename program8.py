24dbd0feb52191ce1757f9170fcd749e417f9fe0
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-14 22:55:08 +0530

    first commit

--- program8.py
+++ program8.py
@@ -14,3 +14,15 @@ for i in range (10,0,-1):
 for i in range (2,21,+2):
     print(i)
     '''
+
+#print odd numbers 1 to 19
+'''
+for i in range(1,20,+2):
+    print(i)
+    '''
+
+#print *****
+i = 1
+while i<=5:
+    print("*")
+    i=i+1
