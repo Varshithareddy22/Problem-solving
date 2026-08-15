a16a55bbc44644fddfc7c3e1f9ac2102057e1b48
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-15 23:04:33 +0530

    first commit

--- program9.py
+++ program9.py
@@ -36,6 +36,7 @@ print(i)
 '''
 
 #sum of digits
+'''
 x= int(input("enter the number:"))
 i=0
 while x>0:
@@ -43,3 +44,13 @@ while x>0:
     i=a+i
     x=x//10
 print(i)    
+'''
+
+#reverse numbers
+x= int(input("enter the number:"))
+reverse = 0
+while x!=0:
+    a=x%10
+    reverse=reverse*10+a
+    x=x//10
+print(reverse)
