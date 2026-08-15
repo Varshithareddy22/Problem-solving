83c3d87412a0ef52ce76a547a38ef53343e0d1fc
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-15 21:33:52 +0530

    first commit

--- program9.py
+++ program9.py
@@ -1,4 +1,5 @@
 #sum of first n numbers
+'''
 x= int(input("enter the number"))
 sum=0
 i=1
@@ -6,3 +7,12 @@ while i<=x:
     sum=sum+i
     i=i+1
 print(sum)
+'''
+#multiplication table
+x= int(input("enter the table:"))
+i=1
+while i<=10:
+    j=x*i
+    print(j)
+    i=i+1
+
