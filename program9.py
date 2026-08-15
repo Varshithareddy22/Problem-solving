76e7c1178559c990813546b68ed7d7b97931f089
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-15 23:44:01 +0530

    first commit

--- program9.py
+++ program9.py
@@ -47,6 +47,7 @@ print(i)
 '''
 
 #reverse numbers
+'''
 x= int(input("enter the number:"))
 reverse = 0
 while x!=0:
@@ -54,3 +55,18 @@ while x!=0:
     reverse=reverse*10+a
     x=x//10
 print(reverse)
+'''
+
+#palindrome number
+x= int(input("enter the number:"))
+original=x
+reverse = 0
+while x!=0:
+    a=x%10
+    reverse=reverse*10+a
+    x=x//10
+if (original==reverse):
+    print(reverse,"is palindrome")
+else:
+    print(reverse,"is not palindrome")
+    
