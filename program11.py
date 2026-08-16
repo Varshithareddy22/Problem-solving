0aa462aa7347c175081b5e81b2edd6b202e12004
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-16 22:45:38 +0530

    first commit

--- program11.py
+++ program11.py
@@ -1,6 +1,17 @@
 #leap year 
+'''
 x=int(input("Enter the year:"))
 if (x%400==0) or (x%100!=0 and x%4==0):
     print(x,"is a leap year")       
 else: 
     print(x,"is not a leap year")    
+'''
+
+#vowles and constants
+x=(input("Enter the letter:"))
+x=x.lower()
+if x in "aeiou":
+    print(x,"is a vowle")
+else:
+    print(x,"is a constant")    
+   
