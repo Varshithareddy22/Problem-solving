fe1541c93a1199c0f1dd421e48fc518532cc9564
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-17 20:03:44 +0530

    Add files via upload

---
+++ calculate_percentage.py
@@ -0,0 +1,7 @@
+# m1=int(input("enter the m1 marks"))
+# m2=int(input("enter the m2 marks"))
+# m3=int(input("enter the m3 marks"))
+# m4=int(input("enter the m4 marks"))
+# m5=int(input("enter the m5 marks"))
+# percentage = (m1+m2+m3+m4+m5)/500*100
+# print(percentage)
