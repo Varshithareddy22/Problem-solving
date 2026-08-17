9321a4176455471a062d3d789c2f797d9eba8294
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-17 20:33:06 +0530

    Add files via upload

---
+++ simple_Interest.py
@@ -0,0 +1,5 @@
+Principal = float(input("Enter the principal in years: "))
+Rate = float(input("Enter the interest rate in % : "))
+Time = float(input("Enter the time: "))
+simple_Interest = (Principal*Rate*Time)/100
+print(simple_Interest)
