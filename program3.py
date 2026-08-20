c86cd6512ddfd7320a44590221d900a26ee551c2
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-20 23:43:22 +0530

    Update program3.py

--- program3.py
+++ program3.py
@@ -3,7 +3,7 @@ x= "Varshitha"
 y= 21
 print("my name is", x , "and I am" , y , "years old.")
 
-#2
+#2 finding age 
 x= int(input("enter current year:"))
 y= int(input("enter your birth year:"))
 a= x-y
