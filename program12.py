3c6f78c80b5dd48b608312ad02d105a162f7743a
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-20 23:42:23 +0530

    Update program12.py

--- program12.py
+++ program12.py
@@ -1,7 +1,7 @@
-#vowles and constants
+#finding vowel and constants
 x=(input("Enter the letter:"))
 x=x.lower()
 if x in "aeiou":
-    print(x,"is a vowle")
+    print(x,"is a vowel")
 else:
     print(x,"is a constant")
