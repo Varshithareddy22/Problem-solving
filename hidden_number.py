1b45183f207f12458a152ef16224cd4aedb4824d
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-21 22:25:12 +0530

    first commit

--- hidden_number.py
+++ hidden_number.py
@@ -3,7 +3,6 @@ guess = int(input("Guess the hidden number: "))
 if guess == hidden_number:
     print("Correct! You guessed the hidden number.")
 elif guess > hidden_number:
-    print("Too high!")
-
+    print("High!")
 else:
-    print("Too low!")
+    print("Low")
