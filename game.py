25c2c6a04db69597c34e8cad702cc10ecae6ae75
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-21 22:18:45 +0530

    first commit

---
+++ game.py
@@ -0,0 +1,14 @@
+#Rock , paper , scissors game 
+import random
+choices = ["rock", "paper", "scissors"]
+user = input("Enter rock, paper, or scissors: ").lower()
+computer = random.choice(choices)
+print("Computer chose:", computer)
+if user == computer:
+    print("It's a tie!")
+elif (user == "rock" and computer == "scissors") or \
+     (user == "paper" and computer == "rock") or \
+     (user == "scissors" and computer == "paper"):
+    print("Player wins!")
+else:
+    print("Computer wins!")
