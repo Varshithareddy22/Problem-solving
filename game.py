9cbd280456e5a15630915986a7c5e733ec76602c
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-21 22:21:57 +0530

    first commit

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ computer = random.choice(choices)
 print("Computer chose:", computer)
 if user == computer:
     print("It's a tie!")
-elif (user == "rock" and computer == "scissors") or \
+elif (user == "rock" and computer == "scissors") or\
      (user == "paper" and computer == "rock") or \
      (user == "scissors" and computer == "paper"):
     print("Player wins!")
