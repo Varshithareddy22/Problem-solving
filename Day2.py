d576c9638e9a63b114d4e2210267a43917078c09
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-22 22:58:41 +0530

    first commit

--- Day2.py
+++ Day2.py
@@ -2,3 +2,12 @@
 #print string
 sentence=input("Enter the sentence:")
 print(sentence)
+
+#length of string
+sentence=input("Enter the sentence:")
+print(sentence)
+print(len(sentence))
+
+#negative indexing
+sentence=input("Enter the sentence:")
+print(sentence(-1))
