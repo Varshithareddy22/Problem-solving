8e2ad515855950baa9842f144405ce033ef83dd0
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-22 22:42:10 +0530

    first commit

--- Day1.py
+++ Day1.py
@@ -2,3 +2,8 @@
  #print list
 movies = ["Dear Comrade", "Arjun Reddy", "RRR"]
 print(movies)
+
+#print list in sequence
+movies = ["Dear Comrade", "Arjun Reddy", "RRR"]
+for movie in movies:
+    print(movie)
