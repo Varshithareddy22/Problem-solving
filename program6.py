916ae751539d71a6d02ad50a3d3f0910c2a86f33
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-26 22:11:52 +0530

    Update program6.py

--- program6.py
+++ program6.py
@@ -1,4 +1,4 @@
-#divisible by 3 or 7
+# divisible by 3 or 7
 x= int(input("enter the variable:"))
 if x%3==0 :
     print(x,"is divisible by 3")
@@ -7,7 +7,7 @@ elif x%7==0 :
 else:
     print(x,"is not divible 3 or 7")     
 
-#divisible by 5 and 11
+# divisible by 5 and 11
 x= int(input("enter the variable:"))
 if x%5==0 and x%11==0:
     print(x,"is divisible by 5 and 11")
