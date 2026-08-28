dfa3fc176a98d94b9f356c8413aa0cdb288a608a
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-28 22:20:17 +0530

    Update program8.py

--- program8.py
+++ program8.py
@@ -1,25 +1,29 @@
 #print 1 to 10
-
 for i in range (1,11):
     print(i)
+
 
 #print 10 to 1
 for i in range (10,0,-1):
     print(i)
 
+
 #print even numbers 2 to 20
 for i in range (2,21,+2):
     print(i)
 
+
 #print odd numbers 1 to 19
 for i in range(1,20,+2):
     print(i)
+
 
 #print *****
 i = 1
 while i<=5:
     print("*")
     i=i+1
+
 
 # pattern
 i = 5
