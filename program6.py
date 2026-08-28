214c1300808c6c6279dd1dde51e2027656a7d5cc
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-28 22:12:12 +0530

    Update program6.py

--- program6.py
+++ program6.py
@@ -7,12 +7,14 @@ elif x%7==0 :
 else:
     print(x,"is not divible 3 or 7")     
 
+
 # divisible by 5 and 11
 x= int(input("enter the variable:"))
 if x%5==0 and x%11==0:
     print(x,"is divisible by 5 and 11")
 else:
     print(x,"is not divible 5 and 11") 
+
 
 #checking if number lies between 10 and 50
 x= int(input("enter the variable:"))
