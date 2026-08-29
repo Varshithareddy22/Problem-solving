0fbd50308d98beea15162b628ca4f081980702b5
Varshitha Reddy <varshitha9843@gmail.com>
2026-08-29 23:21:37 +0530

    Update program7.py

--- program7.py
+++ program7.py
@@ -10,7 +10,7 @@ else:
     print("z is the second largest number")
 
 
-#find greatest and 2nd greatest value
+# find greatest and 2nd greatest value
 x = int(input("enter the value of x:"))
 y = int(input("enter the value of y:"))
 z = int(input("enter the value of z:"))
