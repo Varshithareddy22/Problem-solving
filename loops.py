36bbca4d8461aed169eeb51094b4cf50bfd6dc1a
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-01 22:46:43 +0530

    first commit

--- loops.py
+++ loops.py
@@ -2,13 +2,16 @@
 for a in range (5):
     print(a)
 
+
 #for loop
 for a in range (1,6):
     print(a)
 
+
 #for loop with range in detail
 for a in range (0,20,2):
     print(a)
+
 
 #for loop through a string
 name = "varshitha"
