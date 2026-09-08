d6aad4910a7626fa96c38b27ef0775fb1a09d2f8
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-08 22:24:31 +0530

    first commit

--- loops2.py
+++ loops2.py
@@ -6,7 +6,7 @@ for i in range (1,21):
 #print even numbers
 for i in range (2,50,2):
     print(i)
-'''
+
 #sum of numbers
 sum=0
 i=1
@@ -14,3 +14,9 @@ while i<=100:
     sum=sum+i 
     i=i+1
 print(sum)
+'''
+#multiplication table 
+i=1
+while i<=10:
+    print(5*i)
+    i=i+1
