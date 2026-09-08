d9fbedeb54513b649a8280b5fd484ece77103fcb
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-08 22:59:21 +0530

    first commit

--- functions.py
+++ functions.py
@@ -1,5 +1,13 @@
+'''
 #function1
 def greet():
     print("Hello Varshitha")
 greet()
 greet()
+'''
+#function2
+def greet(name):
+    print("Hello",name)
+greet("Varshitha")
+greet("Smriti")
+greet("Jemi")    
