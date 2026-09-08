8d0c897031fd7d46c887918db14505f8b14f8707
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-08 21:40:17 +0530

    Merge remote changes

--- loops.py
+++ loops.py
@@ -1,18 +1,25 @@
- # for loop 
+'''
+#loop
 for a in range (5):
     print(a)
 
-# for loop 
+
+#for loop
 for a in range (1,6):
     print(a)
 
 
-# for loop with range function printing only even no
+#for loop with range in detail
 for a in range (0,20,2):
     print(a)
  
 
-# for loop through a string to print string
+#for loop through a string
 name = "varshitha"
 for a in name:
     print(a)
+'''
+#infinite loop
+i = 2
+while i <= 5:
+    print(i)
