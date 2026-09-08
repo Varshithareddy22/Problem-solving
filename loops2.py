42a4e03e7a98140261dc2397925ab3c4eab6062d
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-08 22:21:58 +0530

    first commit

--- loops2.py
+++ loops2.py
@@ -2,7 +2,15 @@
 #print numbers
 for i in range (1,21):
     print(i)
-'''
+
 #print even numbers
 for i in range (2,50,2):
     print(i)
+'''
+#sum of numbers
+sum=0
+i=1
+while i<=100:
+    sum=sum+i 
+    i=i+1
+print(sum)
