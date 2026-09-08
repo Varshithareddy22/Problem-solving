6dc28af9dffc31bdb98e0e7cb64e4218defa5496
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-08 21:51:18 +0530

    first commit

--- loops.py
+++ loops.py
@@ -18,8 +18,20 @@ for a in range (0,20,2):
 name = "varshitha"
 for a in name:
     print(a)
-'''
+
 #infinite loop
 i = 2
 while i <= 5:
+    print(i)
+
+#break 
+for i in range (1,10):
+    if i ==6:
+        break
+    print(i)
+'''
+#continue
+for i in range (1,6):
+    if i ==4:
+        continue
     print(i)
