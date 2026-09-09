68dd8c5cbd2e9c4972211e0c003aeb86d7e94dec
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-09 22:41:28 +0530

    Update program7.py

--- program7.py
+++ program7.py
@@ -9,7 +9,6 @@ elif (y > x and y < z) or (y < x and y > z):
 else:
     print("z is the second largest number")
 
-
 # find greatest and 2nd greatest value
 x = int(input("enter the value of x:"))
 y = int(input("enter the value of y:"))
