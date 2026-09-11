305fdcca4d85383e4d8083d1db5b4e1e2804dd27
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-11 21:51:51 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ greet("Varshitha")
 greet("Smriti")
 greet("Jemi")    
 
-#function with multiple inputs and adding two no
+#function with multiple inputs and adding two number
 def add(a,b):
     print(a+b)
 add(2,3)
