9abe5f165775dcf817017feb533163d914343eb2
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-12 22:55:15 +0530

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -6,7 +6,7 @@ print(sentence)
 # length of string
 sentence=input("Enter the sentence:")        
 print(sentence)
-print(len(sentence))
+print(len(sentence))   
                        
 # negative indexing
 sentence=input("Enter the sentence:")
