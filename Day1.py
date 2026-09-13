499b4404592df24a360af551a77b3c4ff0cec690
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-13 21:43:25 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -6,7 +6,7 @@ print(movies)
 # print list in sequence
 movies = ["Dear Comrade", "Arjun Reddy", "RRR"]
 for movie in movies: 
-    print(movie)   
+    print(movie) 
         
 # adding movies
 movies = ["Dear Comrade", "Arjun Reddy", "RRR"]
