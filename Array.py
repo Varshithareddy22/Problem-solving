b90992f800da1a0acb1555c89856970a52f3a9ba
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-17 22:12:06 +0530

    printing array

---
+++ Array.py
@@ -0,0 +1,13 @@
+
+arr=[5,10,15,20,25]
+print(arr)
+print(type(arr))
+print(arr[2])
+
+arr=[5,10,15,20,25]
+for num in arr:
+    print(num)
+
+arr=[5,10,15,20,25]
+for i in range(len(arr)):
+    print(i)
