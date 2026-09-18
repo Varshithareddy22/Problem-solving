f76efb887e495a68bb2e482f2d5dda65433a1cbc
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-18 21:49:13 +0530

    counting even and odds

---
+++ counting_even_odd_nums.py
@@ -0,0 +1,17 @@
+#even
+arr=[2,7,8,5,16,9,10]
+count=0
+for num in arr:
+    if num%2==0:
+        count+=1
+        print(num)        
+print("Total count is", count)
+
+#odd
+arr=[2,7,8,5,16,9,10]
+count=0
+for num in arr:
+    if num%2!=0:
+        count+=1
+        print(num)        
+print("Total count is" ,count)
