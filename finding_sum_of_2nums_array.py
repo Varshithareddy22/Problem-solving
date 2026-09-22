6cdee2a3632aa8092e18e4af8c3d1c80a69d04fd
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-22 22:23:28 +0530

    finding sum of 2 numbers

---
+++ finding_sum_of_2nums_array.py
@@ -0,0 +1,14 @@
+arr=[2,7,11,15]
+target=9
+left=0
+right=len(arr)-1
+while left<right:
+    total=arr[left]+arr[right]
+    if total==target:
+        print(target,"found")
+        break
+    elif total>target:
+        right-=1
+    elif total<target:
+        left+=1
+
