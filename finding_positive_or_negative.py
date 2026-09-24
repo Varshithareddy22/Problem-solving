452f506cf8ea614060bf79b598a4dd0601cad06c
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-24 21:41:53 +0530

    Add files via upload

--- finding_positive_or_negative.py
+++ finding_positive_or_negative.py
@@ -1,8 +1,7 @@
-# finding the number is positive or negative of a no 
-n = int(input("enter the no:"))
-if n>0:    
-    print("no is positive")              
+n = int(input("enter the no: "))
+if n>0:
+    print("no is positive")
 elif n<0:
-    print("no is negative") 
+    print("no is negative")
 else:
     print("It is neutral")
