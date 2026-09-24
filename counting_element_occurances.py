565313971e22a419e843f9274f516a5281a6e360
Varshitha Reddy <varshitha9843@gmail.com>
2026-09-24 21:27:57 +0530

    Update counting_element_occurances.py

--- counting_element_occurances.py
+++ counting_element_occurances.py
@@ -12,7 +12,7 @@ for num in arr:
 print("count of 5 is",count1)    
 print("count of 9 is",count2)
 
-#checking how many times a 1 element is repeated 
+# checking how many times a 1 element is repeated 
 arr=[5,8,5,9,5,1]
 target=5
 count=0
